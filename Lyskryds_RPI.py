8a1ef7cc9441baad9f2a5bfcc5ea2fa9eec32886
deckrd <phlpvst@gmail.com>
2020-09-09 13:32:08 +0200

    crash

--- Lyskryds_RPI.py
+++ Lyskryds_RPI.py
@@ -1,6 +1,7 @@
 #Statemachine til beskrivelse af livets gang
 from gpiozero import LED
 from time import sleep
+import random
 
 NSred= LED(13)
 NSyellow=LED(19)
@@ -38,6 +39,8 @@ def redred(x):# Udgangs punkt for lyskrydset
         EVred.on()
         NSyellow.off()
         EVyellow.off()
+        NSgreen.off()
+        EVgreen.off()
         sleep(2)
         return EV(x)
     elif x=="EV": #Hvis lyskrydset kommer fra EV skal den gå til NS
@@ -47,21 +50,27 @@ def redred(x):# Udgangs punkt for lyskrydset
         EVred.on()
         EVyellow.off()
         NSyellow.off()
+        EVgreen.off()
+        NSgreen.off()
         sleep(2)
         return NS(x)
 def NS(x):
+    global count
     if x == "NS" :
         x = "NS RED/YELLOW"
         print("NS RED/YELLOW   EV red")
+        count = count + 1
         NSred.on()
         NSyellow.on()
         sleep(2)
         return NS_green(x)
 
 def NS_green(x):
+    global count
     if x == "NS RED/YELLOW":
         x = "NS GREEN"
         print("NS GREEN    EV red")
+        count = count + 1
         NSred.off()
         NSyellow.off()
         NSgreen.on()
@@ -69,40 +78,67 @@ def NS_green(x):
         return NS_yellow(x)
 
 def NS_yellow(x):
+    global count
     if x == "NS GREEN":
         x = "NS"
         print("NS YELLOW    EV red")
+        count = count + 1
         NSgreen.off()
         NSyellow.on()
         sleep(2)
         return redred(x)
 def EV(x):
+    global count
     if x == "EV":
         x = "EV RED/YELLOW"
         print("NS red    EV RED/YELLOW")
+        count = count + 1
         EVred.on()
         EVyellow.on()
         sleep(2)
+        crash()
         return EV_green(x)
 
 def EV_green(x):
+    global count
     if x == "EV RED/YELLOW":
         x = "EV GREEN"
         print("NS red    EV GREEN")
+        count = count + 1
         EVred.off()
         EVyellow.off()
         EVgreen.on()
         sleep(2)
+        crash()
         return EV_yellow(x)
 
 def EV_yellow(x):
+    global count
     if x == "EV GREEN":
         x = "EV"
         print("NS red    EV YELLOW")
+        count =  count + 1
         EVgreen.off()
         EVyellow.on()
         sleep(2)
+        crash()
         return redred(x)
+
+def crash():                 #chance for grøn/grøn
+    global count
+    if count > random.randrange(1, 10):
+        EVred.off()
+        EVyellow.off()
+        EVgreen.on()
+        NSred.off()
+        NSyellow.off()
+        NSgreen.on()
+        sleep(2)
+        count = 0
+        return redred("NS")
     
+    
+count = 0
 state=redred(x="EV")
 while state: state=redred(x="EV")
+
