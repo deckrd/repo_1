288aa5d2ead9dfcecf08b6271318c967831e92c2
deckrd <phlpvst@gmail.com>
2020-09-09 10:51:43 +0200

    lyskryds

--- Lyskryds_RPI.py
+++ Lyskryds_RPI.py
@@ -3,32 +3,32 @@ from gpiozero import LED
 from time import sleep
 
 NSred= LED(13)
-NSgul=LED(19)
+NSyellow=LED(19)
 NSgreen=LED(26)
 EVred=LED(21)
 EVyellow=LED(20)
 EVgreen=LED(16)
 
 
-print("Test!")
-EVred.on()
-sleep(1)
-EVgreen.on()
-sleep(1)
-EVyellow.on()
-sleep(1)
-NSred.on()
-sleep(1)
-NSgreen.on()
-sleep(1)
-NSyellow.on()
-sleep(1)
-EVred.off()
-EVgreen.off()
-EVyellow.off()
-NSred.off()
-NSgreen.off()
-NSyellow.off()
+#print("Test!")
+#EVred.on()
+#sleep(1)
+#EVgreen.on()
+#sleep(1)
+#EVyellow.on()
+#sleep(1)
+#NSred.on()
+#sleep(1)
+#NSgreen.on()
+#sleep(1)
+#NSyellow.on()
+#sleep(1)
+#EVred.off()
+#EVgreen.off()
+#EVyellow.off()
+#NSred.off()
+#NSgreen.off()
+#NSyellow.off()
 
 def redred(x):# Udgangs punkt for lyskrydset
     if x=="NS": #Hvis lyskrydset kommer fra NS skal den gå til EV
@@ -36,6 +36,8 @@ def redred(x):# Udgangs punkt for lyskrydset
         print("NS RED   EV RED")
         NSred.on()
         EVred.on()
+        NSyellow.off()
+        EVyellow.off()
         sleep(2)
         return EV(x)
     elif x=="EV": #Hvis lyskrydset kommer fra EV skal den gå til NS
@@ -43,11 +45,13 @@ def redred(x):# Udgangs punkt for lyskrydset
         print("NS red   EV red")
         NSred.on()
         EVred.on()
+        EVyellow.off()
+        NSyellow.off()
         sleep(2)
         return NS(x)
 def NS(x):
     if x == "NS" :
-        x = "NS RED/YELLOW":
+        x = "NS RED/YELLOW"
         print("NS RED/YELLOW   EV red")
         NSred.on()
         NSyellow.on()
@@ -56,46 +60,49 @@ def NS(x):
 
 def NS_green(x):
     if x == "NS RED/YELLOW":
-        x = "NS GREEN":
-            print("NS GREEN    EV red")
-            NSred.off()
-            NSyellow.off()
-            NSgreen.on()
-            sleep(2)
-            return NS_yellow(x)
+        x = "NS GREEN"
+        print("NS GREEN    EV red")
+        NSred.off()
+        NSyellow.off()
+        NSgreen.on()
+        sleep(2)
+        return NS_yellow(x)
 
 def NS_yellow(x):
     if x == "NS GREEN":
-        x = "EV":
-            print("NS YELLOW    EV red")
-            NSgreen.off()
-            NSyellow.on()
-            sleep(2)
-            return redred(x)
-def EV():
+        x = "NS"
+        print("NS YELLOW    EV red")
+        NSgreen.off()
+        NSyellow.on()
+        sleep(2)
+        return redred(x)
+def EV(x):
     if x == "EV":
-        x = "ES RED/YELLOW"
+        x = "EV RED/YELLOW"
         print("NS red    EV RED/YELLOW")
-        ESred.on()
-        ESyellow.on()
+        EVred.on()
+        EVyellow.on()
         sleep(2)
         return EV_green(x)
 
-def EV_green():
-    if x == "EV RED/YELLOW"
+def EV_green(x):
+    if x == "EV RED/YELLOW":
         x = "EV GREEN"
         print("NS red    EV GREEN")
-        ESgreen.on()
+        EVred.off()
+        EVyellow.off()
+        EVgreen.on()
         sleep(2)
         return EV_yellow(x)
 
-def EV_yellow():
-    if x == "EV GREEN"
-        x = "NS"
+def EV_yellow(x):
+    if x == "EV GREEN":
+        x = "EV"
         print("NS red    EV YELLOW")
-        ESyellow.on()
+        EVgreen.off()
+        EVyellow.on()
         sleep(2)
-        return redred()
+        return redred(x)
     
 state=redred(x="EV")
 while state: state=redred(x="EV")
