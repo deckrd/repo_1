510c8b5245340e3d4aba827153d70c77093aea46
deckrd <70883234+deckrd@users.noreply.github.com>
2020-09-09 10:11:58 +0200

    Update Lyskryds_RPI.py

--- Lyskryds_RPI.py
+++ Lyskryds_RPI.py
@@ -6,7 +6,7 @@ NSred= LED(13)
 NSgul=LED(19)
 NSgreen=LED(26)
 EVred=LED(21)
-EVgul=LED(20)
+EVyellow=LED(20)
 EVgreen=LED(16)
 
 
@@ -15,20 +15,20 @@ EVred.on()
 sleep(1)
 EVgreen.on()
 sleep(1)
-EVgul.on()
+EVyellow.on()
 sleep(1)
 NSred.on()
 sleep(1)
 NSgreen.on()
 sleep(1)
-NSgul.on()
+NSyellow.on()
 sleep(1)
 EVred.off()
 EVgreen.off()
-EVgul.off()
+EVyellow.off()
 NSred.off()
 NSgreen.off()
-NSgul.off()
+NSyellow.off()
 
 def redred(x):# Udgangs punkt for lyskrydset
     if x=="NS": #Hvis lyskrydset kommer fra NS skal den gå til EV
@@ -45,17 +45,57 @@ def redred(x):# Udgangs punkt for lyskrydset
         EVred.on()
         sleep(2)
         return NS(x)
-def NS():
+def NS(x):
+    if x == "NS" :
+        x = "NS RED/YELLOW":
+        print("NS RED/YELLOW   EV red")
+        NSred.on()
+        NSyellow.on()
+        sleep(2)
+        return NS_green(x)
 
-def NS_green():
+def NS_green(x):
+    if x == "NS RED/YELLOW":
+        x = "NS GREEN":
+            print("NS GREEN    EV red")
+            NSred.off()
+            NSyellow.off()
+            NSgreen.on()
+            sleep(2)
+            return NS_yellow(x)
 
-def NS_gul():
-
+def NS_yellow(x):
+    if x == "NS GREEN":
+        x = "EV":
+            print("NS YELLOW    EV red")
+            NSgreen.off()
+            NSyellow.on()
+            sleep(2)
+            return redred(x)
 def EV():
+    if x == "EV":
+        x = "ES RED/YELLOW"
+        print("NS red    EV RED/YELLOW")
+        ESred.on()
+        ESyellow.on()
+        sleep(2)
+        return EV_green(x)
 
 def EV_green():
+    if x == "EV RED/YELLOW"
+        x = "EV GREEN"
+        print("NS red    EV GREEN")
+        ESgreen.on()
+        sleep(2)
+        return EV_yellow(x)
 
-def EV_gul():
-
+def EV_yellow():
+    if x == "EV GREEN"
+        x = "NS"
+        print("NS red    EV YELLOW")
+        ESyellow.on()
+        sleep(2)
+        return redred()
+    
 state=redred(x="EV")
 while state: state=redred(x="EV")
